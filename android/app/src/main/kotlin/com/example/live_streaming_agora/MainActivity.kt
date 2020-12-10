51cff7e4ed8c15980fc5fdbbcb08b9690046b2d4
zeel_nakrani3006 <zeel.semicolon@gmail.com>
2020-12-10 15:38:41 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/live_streaming_agora/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.live_streaming_agora
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,2 @@
+// Agora AppId
+const APP_ID = '63dd157ccd6b4ea28294e3b16ec2d0ab';
